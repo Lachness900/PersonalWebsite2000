34324d18443bc70205545d12a5cf851a7da3afa1
Lachness900 <122955235+Lachness900@users.noreply.github.com>
2023-01-27 09:27:55 +1100

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,6 +1,7 @@
 use yew::prelude::*;
 use yew_router::prelude::*;
 
+
 #[derive(Debug, Clone, PartialEq, Routable)]
 enum Route {
     #[at("/")]
@@ -19,10 +20,6 @@ fn navbar() -> Html{
     html!{
             <nav class="navbar navbar-expand-lg bg-primary">
                 <div class="container-fluid">
-                    <a class="navbar-brand">{"Navbar"}</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
@@ -46,18 +43,19 @@ fn navbar() -> Html{
 fn home() -> Html{
     html!{
         <>
-                <NavbarComponent/>
                 <h1 class="header">{"Lachlan's Webpage"}</h1>
+                <NavbarComponent/>
                 <h1>{"Home"}</h1>
-            </>
+                <p>{"This is a collection of stuff I wanted to put on a website"}</p>
+        </>
     }
 }
 
 #[function_component(GamesPage)]
 fn games() -> Html {
     html!(<>
+        <h1 class="header">{"Lachlan's Webpage"}</h1>
         <NavbarComponent/>
-        <p class="header">{"Lachlan's Webpage"}</p>
         <h1>{"Games"}</h1>
         <div class="chessbox">
             <div>
@@ -75,37 +73,50 @@ fn games() -> Html {
 #[function_component(ProjectPage)]
 fn projects() -> Html {
     html!(<>
+        <h1 class="header">{"Lachlan's Webpage"}</h1>
         <NavbarComponent/>
-        <p class="header">{"Lachlan's Webpage"}</p>
-        <h1>{"Games"}</h1>
-        <div class="chessbox">
-            <div>
-                <h3>{"Tactics"}</h3>
-                <iframe src="https://livetactics.chessbase.com" width="400" height="440"></iframe>
-            </div>
-            <div>
-                <h3>{"Challenge Computer"}</h3>
-                <iframe src="https://fritz.chessbase.com" style="width:760px;height:440px"></iframe>
-            </div>      
-        </div>
+        <h1>{"Projects"}</h1>
+        <ul>
+            <li><a href="https://github.com/Lachness900">{"Github Account"}</a></li>
+            <li><a href="https://lachness900.itch.io/waves-of-pygame">{"Waves of Pygame"}</a></li>
+            <li><a href="https://wokwi.com/projects/354815499605834753">{"Ardiuno Connect 4"}</a></li>
+            <li>{"This"}</li>
+        </ul>
+        
     </>)
 }
 
 
+
+
 #[function_component(Main)]
 fn app() -> Html {
-    let button_state = use_state(|| 0);
+    let button_state = use_state(|| 0.0);
+    let button_state_multiplier = use_state(||1.0);
+    
 
     let incr_counter = {
         let button_state = button_state.clone();
-        Callback::from(move |_| button_state.set(*button_state + 1))
+        let button_state_multiplier = button_state_multiplier.clone();
+        Callback::from(
+            move |_| button_state.set(*button_state + (1.0 * *button_state_multiplier))
+        )
     };
 
-    let decr_counter = {
+    let incr_counter5 = {
         let button_state = button_state.clone();
-        Callback::from(move |_| button_state.set(*button_state - 1))
+        let button_state_multiplier = button_state_multiplier.clone();
+        Callback::from(
+            move |_| button_state.set(*button_state + (5.0 * *button_state_multiplier))
+        )
     };
-    
+    let upgrade1 = {
+        let button_state = button_state.clone();
+        let button_state_multiplier = button_state_multiplier.clone();
+        Callback::from(
+            move |_| button_state.set(*button_state_multiplier + 0.2)
+        )
+    };
 
     html! {
         <>
@@ -113,10 +124,16 @@ fn app() -> Html {
                 <Switch<Route> render={switch} />
             </BrowserRouter>
 
-            <p> {"current count: "} {*button_state} </p>
+            <p> {"The number: "} {*button_state} </p>
             <button onclick={incr_counter}> {"+"} </button>
-            <button onclick={decr_counter}> {"-"} </button>
-            
+            if *button_state>10.0{
+                <button onclick={incr_counter5}> {"+5"} </button>
+            }
+            if *button_state>50.0{
+                <p>{"Upgrades"}</p>
+                <button onclick={upgrade1} class="upgrade">{"make clicking stronger"}</button>
+                
+            }
         </>
     }
 }
